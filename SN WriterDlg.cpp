69139c4169bdf8e05f0350751054f62a9b7c00e3
wzb <wangzhibin_x@foxmail.com>
2019-04-23 11:31:48 +0800

    release v1.6.220419

--- SN WriterDlg.cpp
+++ SN WriterDlg.cpp
@@ -30,8 +30,8 @@ static char THIS_FILE[] = __FILE__;
 unsigned short g_iTotalComPort = MAX_COMPORT_COUNT;
 unsigned short g_iComportArray[MAX_COMPORT_COUNT] = {0};
 
-const char *g_strToolVersion = "国家码工具 V1.4.220419";//
-const char *g_strToolVersionNo = "V1.4.220419";
+const char *g_strToolVersion = "国家码工具 V1.6.220419";//
+const char *g_strToolVersionNo = "V1.6.220419";
 const char *g_strToolWrite = "写国家码工具";
 const char *g_strToolDelete = "删除国家码工具";
 const char *g_strOfflineTest = "Offline Test";
